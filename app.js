4ccd81a987939a6e81ddbf08c5cfb719c9ce5089
Chris Gonzales <cm.gonzales@yahoo.com>
2018-05-12 23:26:34 -0700

    added connect flash

--- app.js
+++ app.js
@@ -63,6 +63,12 @@ app.use(expessValidator({
   }
 }));
 
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
